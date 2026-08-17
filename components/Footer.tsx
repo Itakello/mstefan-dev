404b97c6cd3a6c9281a3d0deb39466a013df3890
Massimo Stefan <me@mstefan.dev>
2026-08-17 09:32:29 +0200

    Use a bolder X icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,7 +27,7 @@ export function Footer() {
           <IconLink
             href="https://x.com/itakello"
             label="X"
-            icon={<Icon icon="simple-icons:x" className="size-4 text-black dark:text-white" aria-hidden />}
+            icon={<Icon icon="fa6-brands:x-twitter" className="size-4 text-black dark:text-white" aria-hidden />}
           />
         </div>
       </div>
